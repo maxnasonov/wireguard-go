f7f7602be72e68d7c8de0bea20b5361a4a84b6fa
Maxim Nasonov <maxim@nasonov.me>
2024-06-18 19:44:42 +0700

    Revert "Troubleshoot."
    This reverts commit 7648732fdb07fa3aeac5030e8c627fcb31c560f9.

--- tun/offload_linux_test.go
+++ tun/offload_linux_test.go
@@ -11,8 +11,8 @@ import (
 
 	"golang.org/x/sys/unix"
 	"github.com/maxnasonov/wireguard-go/conn"
-	"github.com/maxnasonov/gvisor/pkg/tcpip"
-	"github.com/maxnasonov/gvisor/pkg/tcpip/header"
+	"gvisor.dev/gvisor/pkg/tcpip"
+	"gvisor.dev/gvisor/pkg/tcpip/header"
 )
 
 const (
